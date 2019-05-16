ac6ebe8faf0df2bcdba97b69dfca9cf1de871af8
Kyle Browning <kylebrowning@me.com>
2019-05-16 13:40:34 -0700

    MOAR NAMESPACE

--- Sources/NIOAPNSJWT/APNSJWTJWT.swift
+++ Sources/NIOAPNSJWT/APNSJWTJWT.swift
@@ -13,8 +13,9 @@
 //===----------------------------------------------------------------------===//
 
 import Foundation
+import CAPNSOpenSSL
 
-public struct JWT: Codable {
+public struct APNSJWT: Codable {
     private struct Payload: Codable {
         /// iss
         public let teamID: String
@@ -66,4 +67,22 @@ public struct JWT: Codable {
         }
         return (digest: digest, fixedDigest: sha256(message: digestAsData))
     }
+    private func sha256(message: Data) -> Data {
+        var context = SHA256_CTX()
+        SHA256_Init(&context)
+
+        var res = message.withUnsafeBytes { buffer in
+            SHA256_Update(&context, buffer.baseAddress, buffer.count)
+        }
+        assert(res == 1, "SHA256_Update failed")
+
+        var digest = Data(count: Int(SHA256_DIGEST_LENGTH))
+        res = digest.withUnsafeMutableBytes { mptr in
+            SHA256_Final(mptr.baseAddress?.assumingMemoryBound(to: UInt8.self), &context)
+        }
+        assert(res == 1, "SHA256_Final failed")
+
+        return digest
+    }
+
 }
--- Tests/NIOAPNSJWTTests/JWTTests.swift
+++ Tests/NIOAPNSJWTTests/JWTTests.swift
@@ -22,7 +22,7 @@ final class JWTTests: XCTestCase {
         let teamID = "8RX5AF8F6Z"
         let keyID = "9N8238KQ6Z"
         let date = Date()
-        let jwt = JWT(keyID: keyID, teamID: teamID, issueDate: date, expireDuration: 10.0)
+        let jwt = APNSJWT(keyID: keyID, teamID: teamID, issueDate: date, expireDuration: 10.0)
         let token = try jwt.getDigest()
 
         let part = token.digest.split(separator: ".")
