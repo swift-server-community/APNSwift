2e3fc112e00c8ff73050e1c76ead7f74fe96a681
Kyle Browning <kylebrowning@me.com>
2019-09-24 11:32:59 -0700

    Update license header.

--- Sources/APNSwift/APNSwiftBearerTokenFactory.swift
+++ Sources/APNSwift/APNSwiftBearerTokenFactory.swift
@@ -1,9 +1,17 @@
+//===----------------------------------------------------------------------===//
 //
-//  APNSwiftBearerTokenFactory.swift
-//  APNSwift
+// This source file is part of the APNSwift open source project
 //
-//  Created by Kyle Browning on 9/24/19.
+// Copyright (c) 2019 the APNSwift project authors
+// Licensed under Apache License v2.0
 //
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of APNSwift project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 
 import Foundation
 import NIO
