555798c3c54744292f79059b2a00941f6a537fe4
Kyle Browning <kylebrowning@me.com>
2019-12-16 09:27:30 -0800

    remove extra option for connection. (#75)

--- Sources/APNSwift/APNSwiftConnection.swift
+++ Sources/APNSwift/APNSwiftConnection.swift
@@ -81,10 +81,14 @@ public final class APNSwiftConnection: APNSwiftClient {
      */
     
     public static func connect(configuration: APNSwiftConfiguration, on eventLoop: EventLoop) -> EventLoopFuture<APNSwiftConnection> {
-        return APNSwiftConnection.connect(configuration: configuration, on: eventLoop, logger: configuration.logger)
+        return connect0(configuration: configuration, on: eventLoop, logger: configuration.logger)
     }
     
-    public static func connect(configuration: APNSwiftConfiguration, on eventLoop: EventLoop, logger: Logger? = nil) -> EventLoopFuture<APNSwiftConnection> {
+    public static func connect(configuration: APNSwiftConfiguration, on eventLoop: EventLoop, logger: Logger) -> EventLoopFuture<APNSwiftConnection> {
+        return connect0(configuration: configuration, on: eventLoop, logger: logger)
+    }
+
+    internal static func connect0(configuration: APNSwiftConfiguration, on eventLoop: EventLoop, logger: Logger?) -> EventLoopFuture<APNSwiftConnection> {
         struct UnsupportedServerPushError: Error {}
         let logger = logger ?? configuration.logger
         logger?.debug("Connection - starting")
