098814925ac43df1973f126cb41a0b3ea9985d15
Jeffrey Macko <mackoj@users.noreply.github.com>
2021-09-16 11:52:18 +0200

    Update Package.swift
    Since you use `.makeClientConfiguration()` and it has been introduce in PR https://github.com/apple/swift-nio-ssl/pull/299 please update your base requirement of `swift-nio-ssl`.
    Please consider release a point update.
    
    Thanks

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.10.1"),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.4.0"),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.14.0"),
         .package(url: "https://github.com/apple/swift-nio-http2.git", from: "1.13.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0"),
