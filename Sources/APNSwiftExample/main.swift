247d9bef5c2196422ee0fa81915361350d013647
Kyle Browning <kylebrowning@me.com>
2022-06-09 07:39:35 -0700

    cleanup

--- Sources/APNSwiftExample/main.swift
+++ Sources/APNSwiftExample/main.swift
@@ -66,23 +66,26 @@ struct AcmeNotification: APNSwiftNotification {
 let alert = APNSwiftAlert(title: "Hey There", subtitle: "Subtitle", body: "Body")
 let apsSound = APNSSoundDictionary(isCritical: true, name: "cow.wav", volume: 0.8)
 let aps = APNSwiftPayload(alert: alert, badge: 0, sound: .critical(apsSound), hasContentAvailable: true)
-let temp = try! JSONEncoder().encode(aps)
-let string = String(bytes: temp, encoding: .utf8)
+
 let notification = AcmeNotification(acme2: ["bang", "whiz"], aps: aps)
+
 let dt = "80745890ac499fa0c61c2348b56cdf735343963e085dd2283fb48a9fa56b0527759ed783ae6278f4f09aa3c4cc9d5b9f5ac845c3648e655183e2318404bc254ffcd1eea427ad528c3d0b253770422a80"
 let apns = APNSwiftConnection(configuration: apnsConfig, logger: logger)
 let apnsProd = APNSwiftConnection(configuration: apnsProdConfig, logger: logger)
 let expiry = Date().addingTimeInterval(5)
-do {
-    try await apns.send(notification, pushType: .alert, to: dt, expiration: expiry, priority: 10)
-    try await apns.send(notification, pushType: .alert, to: dt, expiration: expiry, priority: 10)
-    try await apns.send(notification, pushType: .alert, to: dt, expiration: expiry, priority: 10)
-    /// Overriden environment
-    try await apnsProd.send(aps, to: dt, on: .sandbox)
-} catch {
-    print(error)
+Task {
+    do {
+        try await apns.send(notification, pushType: .alert, to: dt, expiration: expiry, priority: 10)
+        try await apns.send(notification, pushType: .alert, to: dt, expiration: expiry, priority: 10)
+        try await apns.send(notification, pushType: .alert, to: dt, expiration: expiry, priority: 10)
+        /// Overriden environment
+        try await apnsProd.send(aps, to: dt, on: .sandbox)
+        try await httpClient.shutdown()
+        try! group.syncShutdownGracefully()
+        exit(0)
+    } catch {
+        print(error)
+    }
 }
-
-try await httpClient.shutdown()
-try group.syncShutdownGracefully()
-exit(0)
+/// Dont use in production, just here to wait for task to complete
+sleep(5)
