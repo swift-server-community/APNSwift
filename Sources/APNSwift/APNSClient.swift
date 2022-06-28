3bde8e16b8cceb5b873f10b571b69c40f1d91070
FranzBusch <f.busch@apple.com>
2022-06-28 14:53:31 +0200

    Add semantic safe API for alert notifications
    # Motivation
    We want to provide new APIs that are semantically safe when sending alerts to APNs. These APIs should feel native in Swift and not leak too many APIs of the APNs interface.
    
    # Modification
    This PR creates a few new semantic types to represent currency types from the APNs API. Furthermore, the PR adds new types for the alert notification and a convenience method to send alert notifications with the APNSClient.
    
    # Result
    We can now send alert notifications.

--- Sources/APNSwift/APNSClient.swift
+++ Sources/APNSwift/APNSClient.swift
@@ -146,6 +146,78 @@ extension APNSClient {
     /// and the semantic-safe APIs should be used instead.
     ///
     /// - Parameters:
+    ///   - payload: The notification payload.
+    ///
+    ///   - deviceToken: The hexadecimal bytes that identify the user’s device. Your app receives the bytes for this device token
+    ///    when registering for remote notifications.
+    ///
+    ///   - pushType: The value of this header must accurately reflect the contents of your notification’s payload. If there’s a mismatch,
+    ///    or if the header is missing on required systems, APNs may return an error, delay the delivery of the notification, or drop it altogether.
+    ///
+    ///   - apnsID: A canonical UUID that identifies the notification. If there is an error sending the notification,
+    ///    APNs uses this value to identify the notification to your server. The canonical form is 32 lowercase hexadecimal digits,
+    ///    displayed in five groups separated by hyphens in the form 8-4-4-4-12. An example UUID is as follows: 123e4567-e89b-12d3-a456-42665544000.
+    ///    If you omit this, a new UUID is created by APNs and returned in the response.
+    ///
+    ///   - expiration: The date when the notification is no longer valid and can be discarded. If this value is not `none`,
+    ///    APNs stores the notification and tries to deliver it at least once, repeating the attempt as needed if it is unable to deliver the notification the first time.
+    ///    If the value is `immediately`, APNs treats the notification as if it expires immediately and does not store the notification or attempt to redeliver it.
+    ///
+    ///   - priority: The priority of the notification. If you omit this header, APNs sets the notification priority to `immediately`.
+    ///
+    ///   - topic: The topic for the notification. In general, the topic is your app’s bundle ID/app ID.
+    ///    It can have a suffix based on the type of push notification. If you’re using a certificate that supports PushKit VoIP or watchOS complication notifications,
+    ///    you must include this header with bundle ID of you app and if applicable, the proper suffix.
+    ///    If you’re using token-based authentication with APNs, you must include this header with the correct bundle ID and suffix combination.
+    ///
+    ///   - collapseID: An identifier you use to coalesce multiple notifications into a single notification for the user.
+    ///    Typically, each notification request causes a new notification to be displayed on the user’s device.
+    ///    When sending the same notification more than once, use the same value in this header to coalesce the requests.
+    ///    The value of this key must not exceed 64 bytes.
+    ///
+    ///   - deadline: Point in time by which sending the notification to APNs must complete.
+    ///
+    ///   - logger: The logger to use for sending this notification.
+    @discardableResult
+    @inlinable
+    public func send<Payload: Encodable>(
+        payload: Payload?,
+        deviceToken: String,
+        pushType: APNSPushType,
+        apnsID: UUID? = nil,
+        expiration: APNSNotificationExpiration,
+        priority: APNSPriority,
+        topic: String? = nil,
+        collapseID: String? = nil,
+        deadline: NIODeadline,
+        logger: Logger = _noOpLogger
+    ) async throws -> APNSResponse {
+        var byteBuffer = self.byteBufferAllocator.buffer(capacity: 0)
+
+        if let payload = payload {
+            try self.requestEncoder.encode(payload, into: &byteBuffer)
+        }
+
+        return try await self.send(
+            payload: byteBuffer,
+            deviceToken: deviceToken,
+            pushType: pushType.configuration.rawValue,
+            apnsID: apnsID,
+            expiration: expiration.expiration,
+            priority: priority.rawValue,
+            topic: topic,
+            collapseID: collapseID,
+            deadline: deadline,
+            logger: logger
+        )
+    }
+
+    /// Sends a notification to APNs.
+    ///
+    /// - Important: This method exposes the raw API for APNs. In general, this should be avoided
+    /// and the semantic-safe APIs should be used instead.
+    ///
+    /// - Parameters:
     ///   - payload: A ``ByteBuffer`` with the notification payload.
     ///
     ///   - deviceToken: The hexadecimal bytes that identify the user’s device. Your app receives the bytes for this device token
---
+++ Sources/APNSwift/APNSPushType.swift
@@ -0,0 +1,94 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the APNSwift open source project
+//
+// Copyright (c) 2022 the APNSwift project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of APNSwift project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+/// A struct which represents the different supported APNs push types.
+public struct APNSPushType: Hashable {
+    @usableFromInline
+    internal enum Configuration: String, Hashable {
+        case alert
+        case background
+        case location
+        case voip
+        case complication
+        case fileprovider
+        case mdm
+    }
+
+    /// The underlying raw value that is send to APNs.
+    @usableFromInline
+    internal var configuration: Configuration
+
+    /// Use the alert push type for notifications that trigger a user interaction—for example, an alert, badge, or sound.
+    ///
+    /// If the notification requires immediate action from the user, set notification priority to `10`; otherwise use `5`.
+    ///
+    /// The alert push type is required on watchOS 6 and later. It is recommended on macOS, iOS, tvOS, and iPadOS.
+    ///
+    /// - Important: If you set this push type, the topic must use your app’s bundle ID as the topic.
+    public static let alert = Self(configuration: .alert)
+
+    /// Use the background push type for notifications that deliver content in the background, and don’t trigger any user interactions.
+    ///
+    /// The background push type is required on watchOS 6 and later. It is recommended on macOS, iOS, tvOS, and iPadOS.
+    ///
+    /// - Important: If you set this push type, the topic must use your app’s bundle ID as the topic.
+    /// Always use priority `5`. Using priority `10` is an error.
+    public static let background = Self(configuration: .background)
+
+    /// Use the location push type for notifications that request a user’s location.
+    ///
+    /// If the location query requires an immediate response from the Location Push Service Extension, set the notification priority to `10`;
+    /// otherwise, use `5`.
+    ///
+    /// The location push type is recommended for iOS and iPadOS. It isn’t available on macOS, tvOS, and watchOS.
+    ///
+    /// - Important: If you set this push type, the topic must use your app’s bundle ID with `.location-query` appended to the end.
+    ///
+    /// - Important: The location push type supports only token-based authentication.
+    public static let location = Self(configuration: .location)
+
+    /// Use the voip push type for notifications that provide information about an incoming Voice-over-IP (VoIP) call.
+    ///
+    /// The voip push type is not available on watchOS. It is recommended on macOS, iOS, tvOS, and iPadOS.
+    ///
+    /// - Important: If you set this push type, the topic must use your app’s bundle ID with `.voip` appended to the end.
+    ///
+    /// - Important: If you’re using certificate-based authentication, you must also register the certificate for VoIP services.
+    ///  The topic is then part of the 1.2.840.113635.100.6.3.4 or 1.2.840.113635.100.6.3.6 extension.
+    public static let voip = Self(configuration: .voip)
+
+    /// Use the complication push type for notifications that contain update information for a watchOS app’s complications.
+    ///
+    /// The complication push type is recommended for watchOS and iOS. It is not available on macOS, tvOS, and iPadOS.
+    ///
+    /// - Important: If you set this push type, the topic must use your app’s bundle ID with `.complication` appended to the end.
+    ///
+    /// - Important: If you’re using certificate-based authentication, you must also register the certificate for WatchKit services.
+    ///  The topic is then part of the 1.2.840.113635.100.6.3.6 extension.
+    public static let complication = Self(configuration: .complication)
+
+    /// Use the fileprovider push type to signal changes to a File Provider extension.
+    ///
+    /// The fileprovider push type is not available on watchOS. It is recommended on macOS, iOS, tvOS, and iPadOS.
+    ///
+    /// - Important: If you set this push type, the topic must use your app’s bundle ID with `.pushkit.fileprovider` appended to the end.
+    public static let fileprovider = Self(configuration: .fileprovider)
+
+    /// Use the mdm push type for notifications that tell managed devices to contact the MDM server.
+    ///
+    /// The mdm push type is not available on watchOS. It is recommended on macOS, iOS, tvOS, and iPadOS.
+    ///
+    /// - Important: If you set this push type, you must use the topic from the UID attribute in the subject of your MDM push certificate.
+    public static let mdm = Self(configuration: .mdm)
+}
