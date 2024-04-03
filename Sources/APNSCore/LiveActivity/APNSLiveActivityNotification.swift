e20b90a6f696102b6bc240c7b3bd6f2a679bf89d
Pat Nakajima <patnakajima@gmail.com>
2024-04-03 14:24:24 -0700

    Remove unnecessary imports

--- Sources/APNSCore/LiveActivity/APNSLiveActivityNotification.swift
+++ Sources/APNSCore/LiveActivity/APNSLiveActivityNotification.swift
@@ -12,7 +12,6 @@
 //
 //===----------------------------------------------------------------------===//
 
-import struct Foundation.Data
 import struct Foundation.UUID
 
 /// A live activity notification.
--- Sources/APNSCore/LiveActivity/APNSLiveActivityNotificationAPSStorage.swift
+++ Sources/APNSCore/LiveActivity/APNSLiveActivityNotificationAPSStorage.swift
@@ -11,7 +11,6 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 //===----------------------------------------------------------------------===//
-import struct Foundation.Data
 
 struct APNSLiveActivityNotificationAPSStorage<ContentState: Encodable & Hashable & Sendable>:
     Encodable, Sendable, Hashable
